8eafa8fbbfb180e16fe3cfd6683199dd3dc618ee
yash <bhaliyayash595@gmail.com>
2026-06-04 18:04:58 +0530

    chore: remove obsolete project migration and styling utility scripts

--- temp_reload.js
+++
@@ -1,8 +0,0 @@
-const { Client } = require('pg');
-
-async function reload() {
-  const client = new Client({
-    connectionString: 'postgresql://postgres.ukxwejrgfayjlouinpew:bybiyynqdmiappnu@aws-0-ap-south-1.pooler.supabase.com:6543/postgres' // wait, I don't know the new database password. I asked the user for it but they only gave ONE password!
-  });
-  // ...
-}
