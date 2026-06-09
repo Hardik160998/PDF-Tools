b426e6b1106ae5ed85cb3043934ea3707264e4b4
yash <bhaliyayash595@gmail.com>
2026-06-09 18:02:39 +0530

    Add DNS prefetch and preconnect for Desktop FCP

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -66,6 +66,9 @@ export default function RootLayout({
   return (
     <html lang="en" suppressHydrationWarning>
       <head>
+        <link rel="preconnect" href="https://fonts.googleapis.com" />
+        <link rel="preconnect" href="https://fonts.gstatic.com" crossOrigin="anonymous" />
+        <link rel="dns-prefetch" href="https://fonts.googleapis.com" />
         <GoogleAnalytics gaId="G-CJBE2GYFFR" />
         <meta name="color-scheme" content="light dark" />
         <script
